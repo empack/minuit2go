722bdc3a3391cf1e99d058ee52b3c65bedbcfa31
Kian <kian.pasani@gmail.com>
2025-02-04 18:44:27 +0100

    add MnFcn

--- pkg/MnFcn.go
+++ pkg/MnFcn.go
@@ -1,1 +1,32 @@
 package minuit
+
+type MnFcn struct {
+	theFCN      FCNBase
+	theNumCall  int
+	theErrorDef float64
+}
+
+func NewMnFcn(fcn FCNBase, errorDef float64) *MnFcn {
+	return &MnFcn{
+		theFCN:      fcn,
+		theNumCall:  0,
+		theErrorDef: errorDef,
+	}
+}
+
+func (this *MnFcn) valueOf(v *MnAlgebraicVector) float64 {
+	this.theNumCall++
+	return this.theFCN.ValueOf(v.asArray())
+}
+
+func (this *MnFcn) numOfCalls() int {
+	return this.theNumCall
+}
+
+func (this *MnFcn) errorDef() float64 {
+	return this.theErrorDef
+}
+
+func (this *MnFcn) fcn() FCNBase {
+	return this.theFCN
+}
