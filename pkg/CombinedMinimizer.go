73af3eb8dd76cc14e3a47584babe897444280844
Alexander Utterodt <alexander.utterodt@stud.tu-darmstadt.de>
2025-02-04 17:35:32 +0100

    CombinedMinimizer

---
+++ pkg/CombinedMinimizer.go
@@ -0,0 +1,21 @@
+package minuit
+
+type CombinedMinimizer struct {
+	ModularFunctionMinimizer
+	theMinSeedGen *MnSeedGenerator
+	theMinBuilder *CombinedMinimumBuilder
+}
+
+func NewCombinedMinimizer() *CombinedMinimizer {
+	return &CombinedMinimizer{
+		theMinSeedGen: NewMnSeedGenerator(),
+		theMinBuilder: NewCombinedMinimumBuilder(),
+	}
+}
+
+func (this *CombinedMinimizer) SeedGenerator() *MinimumSeedGenerator {
+	return this.theMinSeedGen
+}
+func (this *CombinedMinimizer) Builder() MinimumBuilder {
+	return this.theMinBuilder
+}
