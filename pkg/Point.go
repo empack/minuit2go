f7a3df7daf1eb359ed5f0894a3df8703fae58aa0
Kian <kian.pasani@gmail.com>
2025-02-05 09:42:25 +0100

    add Point

--- pkg/Point.go
+++ pkg/Point.go
@@ -1,1 +1,13 @@
 package minuit
+
+type Point struct {
+	first  float64
+	second float64
+}
+
+func NewPoint(first, second float64) *Point {
+	return &Point{
+		first:  first,
+		second: second,
+	}
+}
