1c8455767d3f7df4e9afd9fc372b2e2269fde08b
Alexander Utterodt <alexander.utterodt@stud.tu-darmstadt.de>
2025-02-06 03:10:13 +0100

    fixed errors in CombinedMinimumBuilder

--- pkg/CombinedMinimumBuilder.go
+++ pkg/CombinedMinimumBuilder.go
@@ -15,20 +15,32 @@ func NewCombinedMinimumBuilder() *CombinedMinimumBuilder {
 }
 
 func (this *CombinedMinimumBuilder) Minimum(fcn *MnFcn, gc GradientCalculator, seed *MinimumSeed, strategy *MnStrategy, maxfcn int, toler float64) (*FunctionMinimum, error) {
-	var min *FunctionMinimum = this.theVMMinimizer.minimize(fcn, gc, seed, strategy, maxfcn, toler)
+	min, err := this.theVMMinimizer.minimize(fcn, gc, seed, strategy, maxfcn, toler)
+	if err != nil {
+		return nil, err
+	}
 
-	if !min.isValid() {
+	if !min.IsValid() {
 		log.Println("CombinedMinimumBuilder: migrad method fails, will try with simplex method first.")
-		var str *MnStrategy = NewMnStrategy(2)
-		var min1 *FunctionMinimum = this.theSimplexMinimizer.minimize(fcn, gc, seed, str, maxfcn, toler)
-		if !min1.isValid() {
+		var str *MnStrategy = NewMnStrategyWithStra(2)
+		min1, err := this.theSimplexMinimizer.minimize(fcn, gc, seed, str, maxfcn, toler)
+		if err != nil {
+			return nil, err
+		}
+		if !min1.IsValid() {
 			log.Println("CombinedMinimumBuilder: both migrad and simplex method fail.")
 			return min1, nil
 		}
-		var seed1 *MinimumSeed = this.theVMMinimizer.SeedGenerator().generate(fcn, gc, min1.userState(), str)
+		seed1, err := this.theVMMinimizer.SeedGenerator().Generate(fcn, gc, min1.UserState(), str)
+		if err != nil {
+			return nil, err
+		}
 
-		var min2 *FunctionMinimum = this.theVMMinimizer.minimize(fcn, gc, seed1, str, maxfcn, toler)
-		if !min2.isValid() {
+		min2, err := this.theVMMinimizer.minimize(fcn, gc, seed1, str, maxfcn, toler)
+		if err != nil {
+			return nil, err
+		}
+		if !min2.IsValid() {
 			log.Println("CombinedMinimumBuilder: both migrad and method fails also at 2nd attempt.")
 			log.Println("CombinedMinimumBuilder: return simplex minimum.")
 			return min1, nil
